c1926d1de5565c649500b390618171f21a7ee1c0
beste <beste@SERCAN>
2025-02-09 19:25:22 +0300

    Access Veri Tabanı ile CRUD işlemleri tamamlandı.

---
+++ 13-AccessVeriTabani/01-Kitaplik_Proje/Form1.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace _01_Kitaplik_Proje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dgvKitaplik = new System.Windows.Forms.DataGridView();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbTur = new System.Windows.Forms.ComboBox();
+            this.txtKitapID = new System.Windows.Forms.TextBox();
+            this.rdbIkinciEl = new System.Windows.Forms.RadioButton();
+            this.txtKitapAd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtYazar = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSayfa = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.rdbPakette = new System.Windows.Forms.RadioButton();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnBul = new System.Windows.Forms.Button();
+            this.txtKitapBul = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnAra = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKitaplik)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvKitaplik
+            // 
+            this.dgvKitaplik.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvKitaplik.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKitaplik.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvKitaplik.Location = new System.Drawing.Point(0, 333);
+            this.dgvKitaplik.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvKitaplik.Name = "dgvKitaplik";
+            this.dgvKitaplik.RowHeadersWidth = 51;
+            this.dgvKitaplik.RowTemplate.Height = 24;
+            this.dgvKitaplik.Size = new System.Drawing.Size(911, 177);
+            this.dgvKitaplik.TabIndex = 0;
+            this.dgvKitaplik.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvKitaplik_CellClick);
+            // 
+            // btnListele
+            // 
+            this.btnListele.Location = new System.Drawing.Point(422, 35);
+            this.btnListele.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(157, 40);
+            this.btnListele.TabIndex = 1;
+            this.btnListele.Text = "Listele";
+            this.btnListele.UseVisualStyleBackColor = true;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kitap ID:";
+            // 
+            // cmbTur
+            // 
+            this.cmbTur.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTur.FormattingEnabled = true;
+            this.cmbTur.Items.AddRange(new object[] {
+            "Hikaye",
+            "Roman",
+            "Şiir",
+            "Tiyatro"});
+            this.cmbTur.Location = new System.Drawing.Point(160, 157);
+            this.cmbTur.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cmbTur.Name = "cmbTur";
+            this.cmbTur.Size = new System.Drawing.Size(216, 29);
+            this.cmbTur.TabIndex = 3;
+            // 
+            // txtKitapID
+            // 
+            this.txtKitapID.Enabled = false;
+            this.txtKitapID.Location = new System.Drawing.Point(160, 33);
+            this.txtKitapID.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtKitapID.Name = "txtKitapID";
+            this.txtKitapID.Size = new System.Drawing.Size(216, 28);
+            this.txtKitapID.TabIndex = 4;
+            // 
+            // rdbIkinciEl
+            // 
+            this.rdbIkinciEl.AutoSize = true;
+            this.rdbIkinciEl.Location = new System.Drawing.Point(160, 195);
+            this.rdbIkinciEl.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rdbIkinciEl.Name = "rdbIkinciEl";
+            this.rdbIkinciEl.Size = new System.Drawing.Size(98, 25);
+            this.rdbIkinciEl.TabIndex = 5;
+            this.rdbIkinciEl.TabStop = true;
+            this.rdbIkinciEl.Text = "İkinci El";
+            this.rdbIkinciEl.UseVisualStyleBackColor = true;
+            this.rdbIkinciEl.CheckedChanged += new System.EventHandler(this.rdbIkinciEl_CheckedChanged);
+            // 
+            // txtKitapAd
+            // 
+            this.txtKitapAd.Location = new System.Drawing.Point(160, 64);
+            this.txtKitapAd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtKitapAd.Name = "txtKitapAd";
+            this.txtKitapAd.Size = new System.Drawing.Size(216, 28);
+            this.txtKitapAd.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 66);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Kitap Ad:";
+            // 
+            // txtYazar
+            // 
+            this.txtYazar.Location = new System.Drawing.Point(160, 95);
+            this.txtYazar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtYazar.Name = "txtYazar";
+            this.txtYazar.Size = new System.Drawing.Size(216, 28);
+            this.txtYazar.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(96, 97);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 21);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Yazar:";
+            // 
+            // txtSayfa
+            // 
+            this.txtSayfa.Location = new System.Drawing.Point(160, 126);
+            this.txtSayfa.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtSayfa.Name = "txtSayfa";
+            this.txtSayfa.Size = new System.Drawing.Size(216, 28);
+            this.txtSayfa.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(97, 128);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 21);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Sayfa:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(112, 159);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 21);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Tür:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(84, 197);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 21);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Durum:";
+            // 
+            // rdbPakette
+            // 
+            this.rdbPakette.AutoSize = true;
+            this.rdbPakette.Location = new System.Drawing.Point(285, 195);
+            this.rdbPakette.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rdbPakette.Name = "rdbPakette";
+            this.rdbPakette.Size = new System.Drawing.Size(91, 25);
+            this.rdbPakette.TabIndex = 14;
+            this.rdbPakette.TabStop = true;
+            this.rdbPakette.Text = "Pakette";
+            this.rdbPakette.UseVisualStyleBackColor = true;
+            this.rdbPakette.CheckedChanged += new System.EventHandler(this.rdbPakette_CheckedChanged);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(422, 87);
+            this.btnKaydet.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(157, 40);
+            this.btnKaydet.TabIndex = 15;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(422, 140);
+            this.btnSil.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(157, 40);
+            this.btnSil.TabIndex = 16;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(422, 195);
+            this.btnGuncelle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(157, 40);
+            this.btnGuncelle.TabIndex = 17;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(655, 37);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(205, 149);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnBul
+            // 
+            this.btnBul.Location = new System.Drawing.Point(681, 254);
+            this.btnBul.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBul.Name = "btnBul";
+            this.btnBul.Size = new System.Drawing.Size(86, 40);
+            this.btnBul.TabIndex = 19;
+            this.btnBul.Text = "Bul";
+            this.btnBul.UseVisualStyleBackColor = true;
+            this.btnBul.Click += new System.EventHandler(this.btnBul_Click);
+            // 
+            // txtKitapBul
+            // 
+            this.txtKitapBul.Location = new System.Drawing.Point(681, 211);
+            this.txtKitapBul.Margin = new System.Windows.Forms.Padding(2);
+            this.txtKitapBul.Name = "txtKitapBul";
+            this.txtKitapBul.Size = new System.Drawing.Size(216, 28);
+            this.txtKitapBul.TabIndex = 20;
+            this.txtKitapBul.TextChanged += new System.EventHandler(this.txtKitapBul_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(592, 214);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 21);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Kitap Ad:";
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(811, 254);
+            this.btnAra.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(86, 40);
+            this.btnAra.TabIndex = 22;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(911, 510);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtKitapBul);
+            this.Controls.Add(this.btnBul);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.rdbPakette);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtSayfa);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtYazar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtKitapAd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rdbIkinciEl);
+            this.Controls.Add(this.txtKitapID);
+            this.Controls.Add(this.cmbTur);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.dgvKitaplik);
+            this.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kitaplık";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKitaplik)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvKitaplik;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbTur;
+        private System.Windows.Forms.TextBox txtKitapID;
+        private System.Windows.Forms.RadioButton rdbIkinciEl;
+        private System.Windows.Forms.TextBox txtKitapAd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtYazar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSayfa;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton rdbPakette;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnBul;
+        private System.Windows.Forms.TextBox txtKitapBul;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnAra;
+    }
+}
+
---
+++ 13-AccessVeriTabani/01-Kitaplik_Proje/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Access veri tabanına bağlanmak için gerekli kod
+using System.Data.OleDb;
+
+namespace _01_Kitaplik_Proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.JET.OLEDB.4.0;Data Source=C:\\Users\\beste\\OneDrive\\Masaüstü\\Kitaplik.mdb");
+
+        void listele()
+        {
+            DataTable dt = new DataTable();
+            OleDbDataAdapter da = new OleDbDataAdapter("select * from Kitaplar", baglanti);
+            da.Fill(dt);
+            dgvKitaplik.DataSource = dt;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listele();
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            listele();
+        }
+
+        string durum = "";
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand("insert into Kitaplar (KitapAd,Yazar,Tur,Sayfa,Durum) values (@p1,@p2,@p3,@p4,@p5)", baglanti);
+            komut.Parameters.AddWithValue("@p1", txtKitapAd.Text);
+            komut.Parameters.AddWithValue("@p2", txtYazar.Text);
+            komut.Parameters.AddWithValue("@p3", cmbTur.Text);
+            komut.Parameters.AddWithValue("@p4", txtSayfa.Text);
+            komut.Parameters.AddWithValue("@p5", durum);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Kitap Sistemi Kaydedildi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            listele();
+        }
+
+        private void rdbIkinciEl_CheckedChanged(object sender, EventArgs e)
+        {
+            durum = "0";
+        }
+
+        private void rdbPakette_CheckedChanged(object sender, EventArgs e)
+        {
+            durum = "1";
+        }
+
+        private void dgvKitaplik_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secilen = dgvKitaplik.SelectedCells[0].RowIndex;
+            txtKitapID.Text = dgvKitaplik.Rows[secilen].Cells[0].Value.ToString();
+            txtKitapAd.Text = dgvKitaplik.Rows[secilen].Cells[1].Value.ToString();
+            txtYazar.Text = dgvKitaplik.Rows[secilen].Cells[2].Value.ToString();
+            cmbTur.Text = dgvKitaplik.Rows[secilen].Cells[3].Value.ToString();
+            txtSayfa.Text = dgvKitaplik.Rows[secilen].Cells[4].Value.ToString();
+
+            if (dgvKitaplik.Rows[secilen].Cells[5].Value.ToString() == "True")
+            {
+                rdbPakette.Checked = true;
+            }
+            else
+            {
+                rdbIkinciEl.Checked = true;
+            }
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand("delete from Kitaplar where KitapID=@p1", baglanti);
+            komut.Parameters.AddWithValue("@p1", txtKitapID.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Kitap Listeden Silindi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            listele();
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand("update Kitaplar set KitapAd=@p1,Yazar=@p2,Tur=@p3,Sayfa=@p4,Durum=@p5 where KitapID=@p6", baglanti);
+            komut.Parameters.AddWithValue("@p1", txtKitapAd.Text);
+            komut.Parameters.AddWithValue("@p2", txtYazar.Text);
+            komut.Parameters.AddWithValue("@p3", cmbTur.Text);
+            komut.Parameters.AddWithValue("@p4", txtSayfa.Text);
+            if (rdbIkinciEl.Checked == true)
+            {
+                komut.Parameters.AddWithValue("@p5", durum);
+            }
+            if (rdbPakette.Checked == true)
+            {
+                komut.Parameters.AddWithValue("@p5", durum);
+            }
+            komut.Parameters.AddWithValue("@p6", txtKitapID.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Kitap Sistemde Güncellendi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            listele();
+        }
+
+        private void btnBul_Click(object sender, EventArgs e)
+        {
+            OleDbCommand komut = new OleDbCommand("select * from Kitaplar where KitapAd=@p1", baglanti);
+            komut.Parameters.AddWithValue("@p1", txtKitapBul.Text);
+            DataTable dt = new DataTable();
+            OleDbDataAdapter da = new OleDbDataAdapter(komut);
+            da.Fill(dt);
+            dgvKitaplik.DataSource = dt;
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            OleDbCommand komut = new OleDbCommand("select * from Kitaplar where KitapAd like '%" + txtKitapBul.Text + "%'", baglanti);
+            DataTable dt = new DataTable();
+            OleDbDataAdapter da = new OleDbDataAdapter(komut);
+            da.Fill(dt);
+            dgvKitaplik.DataSource = dt;
+        }
+
+        private void txtKitapBul_TextChanged(object sender, EventArgs e)
+        {
+            //ÖDEV: TextBox'a yazıldığı zaman kitap arama işlemi gerçekleştiren programı yazınız.
+            OleDbCommand komut = new OleDbCommand("select * from Kitaplar where KitapAd like '%" + txtKitapBul.Text + "%'", baglanti);
+            DataTable dt = new DataTable();
+            OleDbDataAdapter da = new OleDbDataAdapter(komut);
+            da.Fill(dt);
+            dgvKitaplik.DataSource = dt;
+        }
+    }
+}
